d5dd53fbf6c749c035e10e2dada7756cdd64bd67
Janice Chan <janice.chan1288@gmail.com>
2022-07-15 11:13:45 -0400

    commit tree 51d9c9b3db037fd9c983fee1413c6fcbadc0b544

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester # class should be able to initialize with a string URL
+    def initialize(url)
+        @url = url
+    end
+
+    # sends a GET request to the URL passed in on initialization & should return the body of the response
+    def get_response_body
+        uri = URI.parse(@url) #self.url doesn't work
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    # use get_response_body to send a request
+    # then return a Ruby Array or Hash made up of data converted from the response of that request
+    def parse_json
+       JSON.parse(get_response_body)       
+    end
+
+end
